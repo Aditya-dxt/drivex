482ccd4a7ed3266961fdced389dd3f117346c426
Aditya-dxt <adityadxt1910@gmail.com>
2026-03-12 00:09:20 +0530

    fix: connect to minio s3 api port

--- backend/config/minio.js
+++ backend/config/minio.js
@@ -2,6 +2,7 @@ import * as Minio from "minio";
 
 export const minioClient = new Minio.Client({
   endPoint: process.env.MINIO_ENDPOINT,
+  port: 9000, // ✅ IMPORTANT (MinIO S3 API PORT)
   useSSL: process.env.MINIO_USE_SSL === "true",
   accessKey: process.env.MINIO_ACCESS_KEY,
   secretKey: process.env.MINIO_SECRET_KEY,
