da2bba9db3a2af20905271ef13df79f62fb033d2
Aditya-dxt <adityadxt1910@gmail.com>
2026-03-11 23:47:20 +0530

    fix: minio ssl configuration

--- backend/config/minio.js
+++ backend/config/minio.js
@@ -2,7 +2,7 @@ import * as Minio from "minio";
 
 export const minioClient = new Minio.Client({
   endPoint: process.env.MINIO_ENDPOINT,
-  useSSL: true,
+  useSSL: process.env.MINIO_USE_SSL === "true",
   accessKey: process.env.MINIO_ACCESS_KEY,
   secretKey: process.env.MINIO_SECRET_KEY,
 });
